bb30261e390b05b5ec8601fb636938271b857a9b
Palash A. <thisispalash@kdio.xyz>
2024-08-03 03:37:36 +0530

    Revert "Weekend 03 (#3)"
    This reverts commit ca1dde69bf491d043cf4c912b3165899baa56e79.

--- weekend-03/src/components/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Spinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-300"></div>
-    </div>
-  );
-}
